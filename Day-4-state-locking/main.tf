40dba08a45fa3ec1e027e256dfac6cc7985dea6b
root <root@ip-172-31-64-148.ec2.internal>
2026-01-26 12:22:51 +0000

    day-4

--- Day-4-state-locking/main.tf
+++ Day-4-state-locking/main.tf
@@ -3,14 +3,14 @@ resource "aws_instance" "name" {
     instance_type = var.type
     subnet_id = aws_subnet.name.id
     tags = {
-      Name ="dev"
+      Name ="test"
     }
   
 }
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
-      Name = "prod"
+      Name = "test"
     }
   
 }
