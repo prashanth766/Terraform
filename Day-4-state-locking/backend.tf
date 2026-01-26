3f969967683243de41b8fde53486de408dd97f7f
prashanth766 <chirlavanchaprashanth@gmail.com>
2026-01-26 15:44:14 +0530

    locking

--- Day-4-state-locking/backend.tf
+++ Day-4-state-locking/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "jhffdhfdsfdjdjfjfhfdhfd"
     key = "terraform.tfstate"
-    #use_lockfile = true
+    use_lockfile = true
     region = "us-east-1"
     
   }
