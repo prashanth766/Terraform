a61097e8cdb521008472453e8d0582f02ab13768
prashanth766 <chirlavanchaprashanth@gmail.com>
2026-02-03 18:55:23 +0530

    day-8readrep

--- Day-8-readreplica-by-calling-msater/main.tf
+++ Day-8-readreplica-by-calling-msater/main.tf
@@ -15,7 +15,7 @@ resource "aws_db_instance" "read_replicaaaaaa" {
   identifier     = "mysql-read-replicaaa"
   instance_class = "db.t3.micro"
 
-  # ✅ Correct attribute
+  #Terraform data sources and resources may expose different attribute names; for aws_db_instance data sources, the ARN is accessed via db_instance_arn.
   replicate_source_db = data.aws_db_instance.existing_master.db_instance_arn
 
   db_subnet_group_name   = data.aws_db_subnet_group.existing_subnet_group.name
