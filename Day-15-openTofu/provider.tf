344b7979edf4079beefed28b00e824dd2ba0f0e1
prashanth766 <chirlavanchaprashanth@gmail.com>
2026-02-17 19:48:18 +0530

    day-15

--- Day-15-openTofu/provider.tf
+++ Day-15-openTofu/provider.tf
@@ -1,3 +1,11 @@
-provider "aws" {
+# provider "aws" {
   
+# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws" # The source of the AWS provider or "open-tofu/aws" if using OpenTofu
+      version = "~> 5.0"        # Example version constraint, adjust as needed
+    }
+  }
 }
